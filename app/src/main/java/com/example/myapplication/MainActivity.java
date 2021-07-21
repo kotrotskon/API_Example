f2a664dcf0c64db4a319e4bee3bb8077e5c78146
KotrotsKon <kotrotskon@users.noreply.github.com>
2021-07-21 21:05:46 +0300

    asicronus call

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -36,38 +36,42 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         ArrayList<Case> cases = new ArrayList<>();
 
+        String url = "https://api.covid19api.com/dayone/country/Greece";
+
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i=0; i<response.length(); i++){
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Case aCase = new Case(jsonObject);
+                        cases.add(aCase);
+
+                        Log.d("RESPONSE", jsonObject.getString("Date") + ": " + jsonObject.getInt("Active"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.d("RESPONSE", cases.size()+"");
+                Log.d("RESPONSE", cases.get(265).getDate());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(arrayRequest);
+
+        Log.d("RESPONSE", cases.size()+"");
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String country = editText.getText().toString();
 
-                String url = "https://api.covid19api.com/dayone/country/"+country;
 
-                JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        for (int i=0; i<response.length(); i++){
-                            try {
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                Case aCase = new Case(jsonObject);
-                                cases.add(aCase);
-
-                                Log.d("RESPONSE", jsonObject.getString("Date") + ": " + jsonObject.getInt("Active"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        Log.d("RESPONSE", cases.size()+"");
-                        Log.d("RESPONSE", cases.get(265).getDate());
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-
-                queue.add(arrayRequest);
             }
         });
 
